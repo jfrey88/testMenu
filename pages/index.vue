8149d0c23592a03f93aa7f70c67e7c7657ea305c
jfrey88 <contact@djeffrey.fr>
2024-09-12 12:28:10 +0200

    SQDUITZE

--- pages/index.vue
+++ pages/index.vue
@@ -3,28 +3,30 @@
   <v-btn class="ma-3" @click="ouvreMenu2()"> Menu 2 </v-btn>
   <v-btn class="ma-3" @click="ouvreMenu3()"> Menu 3 </v-btn>
 
-  <DialogMenu1 v-model="dialogStatusMenu1" />
-  <DialogMenu2 v-model="dialogStatusMenu2" />
-  <DialogMenu3 v-model="dialogStatusMenu3" />
+  <Menu1 v-model="dialogStatusMenu1" />
+  <Menu2 v-model="dialogStatusMenu2" />
+  <Menu3 v-model="dialogStatusMenu3" />
 </template>
 
+
 <script setup>
+
 const dialogStatusMenu1 = ref(false);
 const dialogStatusMenu2 = ref(false);
 const dialogStatusMenu3 = ref(false);
 
 const ouvreMenu1=()=>{
   console.log('je passe dans ouvreMenu1');
-  dialogStatusMenu1.value=!dialogStatusMenu1;
+  dialogStatusMenu1.value=!dialogStatusMenu1.value;
 }
 
 const ouvreMenu2=()=>{
   console.log('je passe dans ouvreMenu2');
-  dialogStatusMenu2.value=!dialogStatusMenu2;
+  dialogStatusMenu2.value=!dialogStatusMenu2.value;
 }
 const ouvreMenu3=()=>{
   console.log('je passe dans ouvreMenu3');
-  dialogStatusMenu3.value=!dialogStatusMenu3;
+  dialogStatusMenu3.value=!dialogStatusMenu3.value;
 }
 
 
